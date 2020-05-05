832a64e5ec59b4be26d756a1bb784cb1102f073f
Teodora Vodopija <48692987+teodoraa2019@users.noreply.github.com>
2020-05-05 08:56:45 +0200

    ..

---
+++ TeuraBall/Assets/Carobnjak.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Carobnjak : MonoBehaviour {
+
+    int max;
+    int min;
+    int Pokusaj;
+    int MaxBrojPokusaja = 11;
+
+    public Text text;
+    
+    void Start () {
+        StartGame();
+	}
+	
+    void StartGame() {
+        max = 1000;
+        min = 1;
+        NextPokusaj();
+    }
+	
+	public void pokusajLower () {
+        max = Pokusaj;
+        NextPokusaj();
+	}
+
+    public void pokusajHigher()
+    {
+        min = Pokusaj;
+        NextPokusaj();
+    }
+
+    void NextPokusaj () {
+        Pokusaj = Random.Range(min, max);
+        text.text = Pokusaj.ToString();
+        MaxBrojPokusaja = MaxBrojPokusaja - 1;
+        if (MaxBrojPokusaja < 0)
+        {
+            Application.LoadLevel("Win");
+        }
+    }
+}
---
+++ TeuraBall/Assets/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Učitavanje razine:" + name);
+        Application.LoadLevel(name);
+    }
+
+    // Update is called once per frame
+    public void QuitRequest()
+    {
+        Debug.Log("Gašenje!");
+        Application.Quit();
+    }
+}
